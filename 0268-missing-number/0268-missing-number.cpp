3f3f50256b8007b0e2f6e259ae3a46905c285f02
Akshat Tyagi <95146187+Akshatt10@users.noreply.github.com>
2025-03-06 07:50:57 +0530

    Time: 1 ms (25.81%), Space: 21.7 MB (92.61%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,18 +1,34 @@
+// class Solution {
+// public:
+//     int missingNumber(vector<int>& nums) {
+//         int n = nums.size();
+//         int missing = 0;
+//         unordered_map<int,int> mp;
+//         for(int i=0; i<n; i++){
+//             mp[nums[i]]++;
+//         }
+
+//         for(int i=0; i<=n; i++){
+//             if(mp[i] == 0)
+//             missing = i;
+//         }
+
+//         return missing;
+//     }
+// };
+
+
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
-        int missing = 0;
-        unordered_map<int,int> mp;
-        for(int i=0; i<n; i++){
-            mp[nums[i]]++;
+        int expectedSum = (n * (n + 1)) / 2;
+        int actualSum = 0;
+
+        for (int num : nums) {
+            actualSum += num;
         }
 
-        for(int i=0; i<=n; i++){
-            if(mp[i] == 0)
-            missing = i;
-        }
-
-        return missing;
+        return expectedSum - actualSum;
     }
 };
